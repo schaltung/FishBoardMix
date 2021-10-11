34f92c4dc846ee3d78c37bae7b5b72319db80480
Antonio Moreno Daniel <amorenodaniel@interactions.com>
2021-10-11 13:11:39 -0400

    created eval,dev,train output folders. printed the content.

--- v18to70/build.sh
+++ v18to70/build.sh
@@ -25,6 +25,8 @@ TEST_AGES="18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 
 
 NR_SPK_PER_YAGE=12
 
+mkdir -p eval dev train
+
 (
   for AGE in $TEST_AGES; do
     candidates=""
@@ -63,3 +65,9 @@ done
 
 
 rm -f $_tmpfiles
+echo
+for x in train eval dev; do
+  wc -l $x/*
+  echo
+done
+echo "done!"
