f0abe40e196b52f264cad4d6b79097054c78f6bc
Antonio Moreno <amorenodaniel@interactions.com>
2022-03-14 10:35:28 -0400

    added file with random data for reproducibility of shuffle

--- v18to70-2203/build.sh
+++ v18to70-2203/build.sh
@@ -3,7 +3,7 @@
 # Data-preparation script for paritioning FishBoardMix for Speaker-Age.
 #
 # Requirements:
-#     Your [aggregated] corpus in some folder with the files: spk2gender, spk2yage, utt2yage, utt2spk, wav.scp.
+#     Your [aggregated] corpus located in some folder consisting of these 5 files: spk2gender, spk2yage, utt2yage, utt2spk, wav.scp.
 #
 # Usage:
 #  ./build.sh <SOURCE_CORPUS_DIR>
@@ -17,38 +17,58 @@ trap 'rm -f $_tmpfiles ; trap - INT; kill -s INT "$$"' INT
 
 [[ "$#" < 1 ]] && (>&2 echo -e "USAGE:\n $0 <SOURCE_CORPUS_DIR>\n;;; where SOURCE_CORPUS_DIR contains spk2gender, spk2yage, utt2yage, utt2spk, wav.scp." ) && exit 1;
 
+# Directory of aggregated FishBoardMix.
 D=$1
+
+# Age values of interest.
+TEST_AGES=$(seq 18 70)
+# Speakers/age left aside for eval and dev sets.
+NR_SPK_PER_YAGE=12
+
 
 GENDERS="f m"
 CORPORA="swb mx6 fsh"
-TEST_AGES="18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70"
 
-NR_SPK_PER_YAGE=12
 
 mkdir -p eval dev train
 
+# Select $NR_SPK_PER_YAGE different speakers for each $TEST_AGES.
 (
   for AGE in $TEST_AGES; do
     candidates=""
     for GENDER in $GENDERS; do
       for CORPUS in $CORPORA; do
-        candidates="$candidates $(join -1 1 -2 1 $D/spk2yage $D/spk2gender | awk '{print substr($1,0,3),$2,$3,$1}' | grep " $AGE " | grep " $GENDER " | grep $CORPUS | shuf | awk '{print $4}' | head -n 4)"
+        candidates="$candidates $(join -1 1 -2 1 $D/spk2yage $D/spk2gender | awk '{print substr($1,0,3),$2,$3,$1}' | grep " $AGE " | grep " $GENDER " | grep $CORPUS | shuf --random-source=._random_source | awk '{print $4}' | head -n 4)"
       done
     done
-    echo $candidates | sed -e "s/ /\n/g" | shuf | head -n $NR_SPK_PER_YAGE | awk '{print NR, $0}'
+    echo $candidates \
+      | sed -e "s/ /\n/g" \
+      | shuf --random-source=._random_source \
+      | head -n $NR_SPK_PER_YAGE \
+      | awk '{print NR, $0}'
   done
 ) > .spklist$$
 
 # EVAL,DEV
-cat .spklist$$ | awk '$1<=6 {print $2}' | sort > eval/.spklist$$
-cat .spklist$$ | awk '$1>6 {print $2}' | sort > dev/.spklist$$
+cat .spklist$$ \
+  | awk '$1<=6 {print $2}' \
+  | sort > eval/.spklist$$
+cat .spklist$$ \
+  | awk '$1>6 {print $2}' \
+  | sort > dev/.spklist$$
 
 # overwrite:
-cat eval/.spklist$$ dev/.spklist$$ | sort > .spklist$$
-cat $D/spk2gender | awk '{print $1}' | sort > .tot-spklist$$
+cat eval/.spklist$$ dev/.spklist$$ \
+  | sort > .spklist$$
+cat $D/spk2gender \
+  | awk '{print $1}' \
+  | sort > .tot-spklist$$
 
 # TRAIN
-diff .spklist$$ .tot-spklist$$ | grep '>' | awk '{print $2}' | sort > train/.spklist$$
+diff .spklist$$ .tot-spklist$$ \
+  | grep '>' \
+  | awk '{print $2}' \
+  | sort > train/.spklist$$
 
 
 
@@ -56,11 +76,20 @@ for part in dev eval train; do
     join -1 1 -2 1 $part/.spklist$$ $D/spk2yage > $part/spk2yage
     join -1 1 -2 1 $part/.spklist$$ $D/spk2gender > $part/spk2gender
 
-    cat $D/utt2spk | sort -k2 | join -1 2 -2 1 - $part/spk2yage  | awk '{print $2,$1}' | sort > $part/utt2spk
-    cat $D/utt2spk | sort -k2 | join -1 2 -2 1 - $part/spk2yage  | awk '{print $2,$3}' | sort > $part/utt2yage
+    cat $D/utt2spk \
+      | sort -k2 \
+      | join -1 2 -2 1 - $part/spk2yage  \
+      | awk '{print $2,$1}' \
+      | sort > $part/utt2spk
+    cat $D/utt2spk \
+      | sort -k2 \
+      | join -1 2 -2 1 - $part/spk2yage \
+      | awk '{print $2,$3}' \
+      | sort > $part/utt2yage
 
-    join -1 1 -2 1 $D/wav.scp $part/utt2spk | sed -e "s/|.*/|/" | sort > $part/wav.scp
-
+    join -1 1 -2 1 $D/wav.scp $part/utt2spk \
+      | sed -e "s/|.*/|/" \
+      | sort > $part/wav.scp
 done
 
 
